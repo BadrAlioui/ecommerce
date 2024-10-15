d2ce726244b12a38ed81a604d3e26a2d25a524a7
Badr Aliloui <tranquil2021@hotmail.com>
2024-10-15 15:15:24 +0100

    add css, js, and media

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('post/', include('blog.urls')),
-    
-]
+] 
+
