8967e689e83b901637e7d96ee932bda79427738f
Badr Aliloui <tranquil2021@hotmail.com>
2024-10-15 15:51:22 +0100

    added model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=120, unique=True)
+    
+
+    def __str__(self):
+        return self.title
+
+
+
